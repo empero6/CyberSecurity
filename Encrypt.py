12cbe4297ccef060aa0747c2846c961ab246824f
empero6 <empero6@gmail.com>
2019-10-14 15:49:41 -0400

    Add files via upload

--- Encrypt.py
+++ Encrypt.py
@@ -1,23 +1,87 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QFileDialog, QPushButton, QDialog,QVBoxLayout
+from PyQt5.QtCore import pyqtSlot
 import sys
-from PyQt5.QtGui import QIcon
+import PyQt5.QtGui
+from PyQt5.QtGui import QPixmap
 
 class App(QWidget):
-
-# Initiates the general space of the window
     def __init__(self):
         super().__init__()
-        self.title = 'Encryptor/Decryptor'
+        self.title = 'Encryptor/Decryptor Dialogue'
         self.left = 10
         self.top = 10
         self.width = 640
         self.height = 480
         self.initUI()
 
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        vBox = QVBoxLayout()
+
+
+        self.btn = QPushButton('File Dialogue')
+        self.btn.setFont(PyQt5.QtGui.QFont('Sanserif', 15))
+        self.btn.clicked.connect(self.open_FileDialog)
+        self.btn.clicked.connect(self.getImage)
+
+        vBox.addWidget(self.btn)
+        self.setLayout(vBox)
+
+        #
+        # self.openFileNameDialog()
+        # self.openFileNamesDialog()
+        # self.saveFileDialog()
+
         self.show()
+
+    def open_FileDialog(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.openFileNameDialog()
+        self.openFileNamesDialog()
+        self.saveFileDialog()
+
+        self.show()
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()',
+                                                'Image Files (*);; Image Files (*.jpeg)',
+                                                options=options)
+        if fileName:
+            print(fileName)
+
+    def openFileNamesDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _ = QFileDialog.getOpenFileNames(self, 'QFileDialog.getOpenFileNames()',
+                                                '',
+                                                'Image Files (*);; Image Files(*.jpeg)',
+                                                options=options)
+        if files:
+            print(files)
+
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self, 'QFileDialog.getSaveFileName()',
+                                                  '',
+                                                  'Image Files (*);; Image Files (*jpeg)',
+                                                  options=options)
+        if fileName:
+            print(fileName)
+
+    def getImage(self):
+        file_Name = QFileDialog.getOpenFileName(self, 'Open',
+                                                '/users/stephenagbenu')
+        imagePath = file_Name[0]
+        pixmap = QPixmap(imagePath)
+        self.label.setPixmap(QPixmap(pixmap))
+        self.resize(pixmap.width(), pixmap.height())
 
 
 if __name__ == '__main__':
